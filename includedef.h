c680a6f839d86a2d9fa429594bee8b7763bb28b7
Sam Alexe <SamAlexe@users.noreply.github.com>
2014-12-19 16:33:57 -0600

    includedef.h

---
+++ includedef.h
@@ -0,0 +1,15 @@
+#ifndef __INCLUDEDEF_H__
+#define __INCLUDEDEF_H__
+
+
+#include "definition.h"
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <limits>
+#include <vectors>
+#include <cmath>
+
+
+#endif
