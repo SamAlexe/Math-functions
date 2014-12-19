2bb645417b4a302989a3ff6f9419c4d85554aedf
Sam Alexe <170023@glenbrook225.org>
2014-12-19 16:10:07 -0600

    Update trig-function.cpp

--- trig-function.cpp
+++ trig-function.cpp
@@ -1,1 +1,27 @@
-dfdfd
+
+#include "trig-function.h"
+#include <cmath>
+#include "includesdef.h"
+#include <string>
+#include <limits>
+
+//taylor series
+trig::trigsin(int value)
+{
+  
+}
+trig::trigcos(int value)
+{
+
+}
+
+trig::trigtan(int value)
+{
+  //summation
+  for(int i = 0;i < INFINITY; i++)
+  {
+    pow()/
+  }
+  cout << "Tan of Value: " << value << 
+  
+}
