00d1e6953bcaee26a35619068ca35f7991384302
Sam Alexe <SamAlexe@users.noreply.github.com>
2014-12-19 17:21:22 -0600

    Trig.h

---
+++ Trigonometry/Trig.h
@@ -0,0 +1,14 @@
+#ifndef __TRIG_H__
+#define __TRIG_H__
+
+
+class trig
+{
+    public:
+    float funcsin(int value);
+    float funccos(int value);
+    float functan(int value);
+    private:
+    int sum;
+}
+#endif
