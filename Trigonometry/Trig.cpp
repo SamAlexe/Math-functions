30beac2c9aa001803d72a805dc68de68474aaeb1
Sam Alexe <SamAlexe@users.noreply.github.com>
2014-12-19 19:41:48 -0600

    Update Trig.cpp

--- Trigonometry/Trig.cpp
+++ Trigonometry/Trig.cpp
@@ -3,11 +3,26 @@
 
 float trig::funcsin(int value)
 {
-    
+    sum = 0;
+    //summation
+    for(int i = 0; i < INFINITY; i++)
+    {
+        dom = pow(value, 2*i - 1); 
+        num = 2 *i+ 1
+        num = factorial(num);
+        sum += (dom/num);
+    }
+    return sum;
 }
-float trig::funccos()
+float trig::funccos(int value)
 {
-
+    sum = 0;
+    for(int i = 0;i < INFINITY;i++)
+    {
+        dom = pow(value, 2*i);
+        num = 2*i;
+        num = factorial(num);
+        sum += pow((-1), i) * (dom/num);
 }
 float trig::functan()
 {
