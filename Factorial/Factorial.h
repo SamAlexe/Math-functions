3af41d0f3292ae8c8a1b9b26845b21f3ec8f3dbf
Sam Alexe <SamAlexe@users.noreply.github.com>
2014-12-19 19:44:14 -0600

    Factorial Program

---
+++ Factorial/Factorial.h
@@ -0,0 +1,13 @@
+
+#ifndef __FACTORIAL_H__
+#define __FACTORIAL_H__
+#include "include.h"
+
+class factorial
+{
+    public:
+    int factorial(int value);
+    private:
+    int factorial = 0;
+};
+#endif
