f88b383e553eabb6acf208b0f0999433325acadf
Sam Alexe <SamAlexe@users.noreply.github.com>
2014-12-24 01:08:31 -0600

    Update bernoulli.h

--- bernoulli/bernoulli.h
+++ bernoulli/bernoulli.h
@@ -3,9 +3,12 @@
 #define __BERNOULLI_H__
 
 //|Bn| = 2n!/((2π)^n) * ζ(n)
-float B_N(int n);
-
-//\sum_{n=1}^\infty\frac{1}{n^s}
-float zeta(int n);
+class B_n
+{
+  public:
+  float B_N(int n);
+  //\sum_{n=1}^\infty\frac{1}{n^s}
+  float zeta(int n);
+}
 
 #endif
